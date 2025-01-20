5241d0cd39b4fe9db890add71c9b6a8359090e68
Shahin Zangenehpour <shahinz@mac.com>
2025-01-20 01:12:14 -0800

    Create Color+Extensions.swift

---
+++ WeatherNow/Shared/Color+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extensions.swift
+//  WeatherNow
+//
+//  Created by Shahin Zangenehpour on 2025-01-20.
+//
+
+import SwiftUI
+import UIKit
+
+extension Color {
+    static let customGray = Color(red: 242.0/255.0, green: 242.0/255.0, blue: 242.0/255.0)
+}
